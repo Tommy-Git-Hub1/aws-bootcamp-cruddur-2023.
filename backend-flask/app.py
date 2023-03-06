7a6f6e35ddff3517bb05086c561245f2e20cc8b4
Tommy-Git-Hub1 <Tommyadeleke2021@gmail.com>
2023-03-06 21:41:51 +0000

    implement rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -31,6 +31,15 @@ import watchtower
 import logging
 from time import strftime
 
+
+#RollBar
+import os
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
+
+
 # CloudWatch logs Configuring Logger
 #LOGGER = logging.getLogger(__name__)
 #LOGGER.setLevel(logging.DEBUG)
@@ -57,11 +66,10 @@ tracer = trace.get_tracer(__name__)
 
 
 
-
 app = Flask(__name__)
 
 #x-ray
-XRayMiddleware(app, xray_recorder)
+#XRayMiddleware(app, xray_recorder)
 
 
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -84,6 +92,33 @@ cors = CORS(
     #timestamp = strftime('[%Y-%b-%d %H:%M]')
     #LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
     #return response
+
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"\
+
+#rollbar
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+
+
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
