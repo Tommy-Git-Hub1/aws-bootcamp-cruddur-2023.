882e0aa8976fdbc6a06af300b8b1cf271258fa1a
Tommy-Git-Hub1 <Tommyadeleke2021@gmail.com>
2023-08-04 21:43:52 +0000

    changed seed data

--- backend-flask/bin/db/seed.sql
+++ backend-flask/bin/db/seed.sql
@@ -1,7 +1,9 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Andrew Brown','Hafisadeleke97@gmail.com', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko','tadeleke1305@gmail.com', 'bayko' ,'MOCK');
+  ('Andrew Bayko','tadeleke1305@gmail.com', 'bayko' ,'MOCK'),
+  ('Andrew Bayko','Abrown@gmail.com', 'abeezxy' ,'MOCK'),
+  ('Andrew Bayko','Abayko@gmail.com', 'baykoleezy' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
@@ -9,4 +11,4 @@ VALUES
     (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
-  )
+  );
